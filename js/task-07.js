13f4736fe273c84ab22688397c43d3d00dc5f355
lesiastavniichuk <115650219+lesiastavniichuk@users.noreply.github.com>
2023-03-30 13:26:05 +0100

    30.03.2023 13:26

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+
+input.addEventListener("input", (event) => {
+  span.style.fontSize = `${event.target.value}px`;
+});
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+const handleClick = () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  span.textContent = randomColor;
+};
+
+button.addEventListener("click", handleClick);
