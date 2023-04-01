72a961d17a18c91650d7255b3dce32e2e5ce032e
lesiastavniichuk <115650219+lesiastavniichuk@users.noreply.github.com>
2023-04-01 21:21:55 +0100

    01.04.2023

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
-const list = document.querySelector("ul#categories");
-const listItem = list.querySelectorAll("li.item");
-console.log("Number of categories: " + listItem.length);
+// const list = document.querySelector("ul#categories");
+// const listItem = list.querySelectorAll("li.item");
+// console.log("Number of categories: " + listItem.length);
 
 // const items = document.querySelectorAll(".item");
 // items.forEach((item) => {
@@ -9,10 +9,19 @@ console.log("Number of categories: " + listItem.length);
 //   console.log("Elements: " + itemsElements.querySelectorAll("li").length);
 // });
 
-const items = document.querySelectorAll(".item");
-items.forEach(function (item) {
-  const elementHeadingOfCategory = item.querySelector("h2");
-  console.log("Category:", elementHeadingOfCategory.textContent);
-  const elementsInCategorySum = item.querySelectorAll("li");
-  console.log("Elements:", elementsInCategorySum.length);
+// const items = document.querySelectorAll(".item");
+// items.forEach(function (item) {
+//   const elementHeadingOfCategory = item.querySelector("h2");
+//   console.log("Category:", elementHeadingOfCategory.textContent);
+//   const elementsInCategorySum = item.querySelectorAll("li");
+//   console.log("Elements:", elementsInCategorySum.length);
+// });
+
+const listCategoriesEl = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${listCategoriesEl.length}`);
+
+listCategoriesEl.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
